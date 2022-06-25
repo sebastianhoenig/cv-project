338ea7d2b1f637ce303f06249b50c4c66be908de
Sebastian Hönig <hoenigsebastian@gmail.com>
2022-06-25 16:12:38 +0200

    improved output styling

--- src/components/OutputProjectItem.js
+++ src/components/OutputProjectItem.js
@@ -1,13 +1,30 @@
 import React from 'react'
+import '../components-css/OutputProjectItem.css'
 
 function OutputProjectItem( {content} ) {
+
+  const transformedSummary = content.summary.map((el)=>{
+    return <li>{el}</li>;
+  });
+
+  const transformedLink = content.link.substring(0,7) !== 'http://' ? 'http://' + content.link: content.link;
+
   return (
-    <div>
-      {content.name} <br />
-      {content.link} <br />
-      {content.startDate} <br />
-      {content.endDate} <br />
-      {content.summary} <br />
+    <div className="item">
+      <div className="headline-project">
+        <div className="name">
+          {content.name}
+        </div>
+        <div className="linkDiv">
+          <a className="link" href={transformedLink} target="_blank">Github Link</a>
+        </div>
+        <div className="timerange">
+          {content.startDate} - {content.endDate} 
+        </div>
+      </div>
+      <ul>
+        {transformedSummary}
+      </ul>
     </div>
   )
 }
--- src/components/OutputProjects.js
+++ src/components/OutputProjects.js
@@ -1,17 +1,45 @@
 import React, { useContext } from 'react';
 import { CvContext } from '../context/CvContext';
 import OutputProjectItem from './OutputProjectItem';
+import '../components-css/OutputProject.css'
 
 function OutputProjects() {
   // nicht das deleten, sondern die <ExperienceItem />
   const cv = useContext(CvContext)[0];
+
+  const project = [
+    {
+      name: "CV-Creator",
+      startDate: "03/2022",
+      endDate: "05/2022",
+      link: "www.github.com/sebastianhoenig",
+      summary: [
+        "Created a program to style and format a CV template and used it to write this CV.", 
+        "Used Technologies: HTML, CSS, ReactJS."
+      ]
+      }
+  ]
   return (
-    <div> 
-      {cv.project.map((element, idx) => {
-        return <OutputProjectItem key={idx} content={element}/>
-      })}
+    <div className="outputProjectStart"> 
+      <div className="projectHeadline">
+        <h4>Projects</h4>
+        <hr />
+      </div>
+      <div className="outputProject">
+        {project.map((element, idx) => {
+          return <OutputProjectItem key={idx} content={element}/>
+        })}
+      </div>
     </div>
   )
 }
 
 export default OutputProjects
+
+/**
+ *  {content.name} <br />
+      {content.link} <br />
+      {content.startDate} <br />
+      {content.endDate} <br />
+      {content.summary} <br />
+ */
